50efc13626f11590a3831f5e818913bbb52a0e79
Subhashree naik <subhashree.naik@gmail.com>
2022-02-15 23:05:33 +0530

    calculate daily wage

--- src/com/bridgelab/practiceday4/EmployeeWagePart.java
+++ src/com/bridgelab/practiceday4/EmployeeWagePart.java
@@ -2,26 +2,39 @@ package com.bridgelab.practiceday4;
 
 public class EmployeeWagePart {
 
-	//This method checks attendance of employee
-		 void checkEmployee() {
+	//Declaring constant
+	final static int WAGE_PER_HOUR=20;
+	final int FULL_DAY_HOUR=8;
+	
+	//Variable declaring
+	static int working_hour=0;
+	
+	//This method compute daily wage of employee
+	 int computeDailyWage() {
 
-	        int empType = (int) (Math.random() * 10) % 2;
-	        if (empType == 1)
-	        {
-	            System.out.println("Employee is Present");
+        int empType = (int) (Math.random() * 10) % 2;
+        if (empType == 1)
+        {
+            System.out.println("Employee is Present");
+            working_hour=8;
 
-	        } else
-	        {
-	            System.out.println("Employee is Absent");
+        } else
+        {
+            System.out.println("Employee is Absent");
+            working_hour=0;
 
-	        }
-		}
-		
-		
+        }
+        int wage = working_hour*WAGE_PER_HOUR;
+        
+		return wage;
+	}
+	
+	
 
-	 public static void main(String[] args) {
-		 EmployeeWagePart emp = new EmployeeWagePart();
-		 emp.checkEmployee();  	
-		        
-		    }
-		}
+ public static void main(String[] args) {
+	 
+	 EmployeeWagePart emp=new EmployeeWagePart();
+	 System.out.println("Employee Daily Wage is " + emp.computeDailyWage());
+	        
+	    }
+	}
