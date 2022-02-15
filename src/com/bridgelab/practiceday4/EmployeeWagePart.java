8a631785ca97a5d45c361dd9c368e9f41d75feb7
Subhashree naik <subhashree.naik@gmail.com>
2022-02-15 22:42:10 +0530

    check Employee

--- src/com/bridgelab/practiceday4/EmployeeWagePart.java
+++ src/com/bridgelab/practiceday4/EmployeeWagePart.java
@@ -2,12 +2,26 @@ package com.bridgelab.practiceday4;
 
 public class EmployeeWagePart {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-     System.out.println("Employee\r\n"
-     		+ "Wage\r\n"
-     		+ "Computation\r\n"
-     		+ "Problem Part2");
-	}
+	//This method checks attendance of employee
+		 void checkEmployee() {
 
-}
+	        int empType = (int) (Math.random() * 10) % 2;
+	        if (empType == 1)
+	        {
+	            System.out.println("Employee is Present");
+
+	        } else
+	        {
+	            System.out.println("Employee is Absent");
+
+	        }
+		}
+		
+		
+
+	 public static void main(String[] args) {
+		 EmployeeWagePart emp = new EmployeeWagePart();
+		 emp.checkEmployee();  	
+		        
+		    }
+		}
