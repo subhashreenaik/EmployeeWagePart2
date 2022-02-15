5a10351a05c4a379bf8574964bfca9ce775b7d6a
Subhashree naik <subhashree.naik@gmail.com>
2022-02-15 20:52:45 +0530

    welcome message

---
+++ src/com/bridgelab/practiceday4/EmployeeWagePart.java
@@ -0,0 +1,13 @@
+package com.bridgelab.practiceday4;
+
+public class EmployeeWagePart {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("Employee\r\n"
+     		+ "Wage\r\n"
+     		+ "Computation\r\n"
+     		+ "Problem Part2");
+	}
+
+}
